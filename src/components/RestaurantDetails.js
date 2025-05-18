003859fb976278287b3b7f3e406daf7acf7697ab
Debashis Moharana <moharana.debashis@gmail.com>
2025-05-18 20:49:05 +0530

    style: enhance RestaurantDetails component with Tailwind CSS for improved layout and styling

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -6,14 +6,17 @@ const RestaurantDetails = () => {
   const { restaurantDetails, menuItems } = useRestaurantDetails(resId);
 
   return (
-    <div className="restaurant-details">
-      <h1>{restaurantDetails?.name}</h1>
+    <div className="flex flex-col items-center m-2">
+      <h1 className="text-xl font-bold">{restaurantDetails?.name}</h1>
       <h4>{restaurantDetails?.cuisines.join(", ")}</h4>
-      <ul className="menu-items">
+      <ul className="flex flex-col flex-wrap w-10/12 items-center mt-4">
         {menuItems?.map((item) => {
           return (
-            <li key={item?.card?.info?.id}>
-              <p>
+            <li
+              key={item?.card?.info?.id}
+              className="flex flex-col w-9/12 justify-center border-b-2 border-gray-300 p-2 items-center"
+            >
+              <p className="p-1 text-lg font-semibold">
                 {item?.card?.info?.name} - ₹
                 {item?.card?.info?.price / 100 ||
                   item?.card?.info?.defaultPrice / 100}
